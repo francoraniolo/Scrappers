f74a609dc38da98b2a6b08c7b8b7d7321b8d76d1
francoraniolo <franco11-09@hotmail.com>
2020-01-12 12:18:36 -0300

    Primer commit

---
+++ ScraperProject/scrape.js
@@ -0,0 +1,48 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+var input='pokemon sword';
+
+var input = input.split(' ').join('-');
+
+console.log('El input es ',input );
+
+var principiourl='https://listado.mercadolibre.com.ar/';
+
+var url = principiourl.concat(input);
+
+request(url, (error,
+    response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        const siteHeading = $('#searchResults');
+
+        var arreglo = input.split('-');
+
+        var existe;
+
+
+        var $el, $titulo, $titulo_minus;
+
+        siteHeading.find('li').each(function () {
+            existe = true;
+            $el = $(this);
+            $titulo =
+                $titulo_minus = $el.find('span.main-title').text().toLowerCase();
+
+            arreglo.forEach(function(entrada) {
+
+                if (existe) existe = $titulo_minus.includes(entrada);
+
+            })
+
+            if (existe) {
+                console.log('Titulo: ', $el.find('span.main-title').text());
+
+                console.log('Precio: ', $el.find('span.price__fraction').text());
+            }
+
+        })
+    }
+});
