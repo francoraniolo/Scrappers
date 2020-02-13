ec00e51a687972eb5706f4f09f4d4fed646bd6e6
francoraniolo <franco11-09@hotmail.com>
2020-02-12 21:03:58 -0300

    estiloViejo en backend

--- ScraperProject/server/articleCreator.js
+++ ScraperProject/server/articleCreator.js
@@ -10,66 +10,66 @@ async function createArticle(url) {
                 const $ = cheerio.load(html);
                 var estiloViejo = false;
 
-                 //Busco la imagen 
-                
-                 var imagenLink = $('a.gallery-trigger').attr('href');
-                 if(imagenLink==null) {
-                     estiloViejo=true;
-                     imagenLink=$('img.ui-pdp-gallery__figure__image').attr('data-zoom');
-                    }
+                //Busco la imagen 
+
+                var imagenLink = $('a.gallery-trigger').attr('href');
+                if (imagenLink == null) {
+                    estiloViejo = true;
+                    imagenLink = $('img.ui-pdp-gallery__figure__image').attr('data-zoom');
+                }
 
                 //Busco el titulo
 
-                if(estiloViejo){
-                   var titulo = $('h1.ui-pdp-title').text();
-                }else{
-                    var titulo = $('header.item-title').find('h1.item-title__primary').text();   
+                if (estiloViejo) {
+                    var titulo = $('h1.ui-pdp-title').text();
+                } else {
+                    var titulo = $('header.item-title').find('h1.item-title__primary').text();
                 }
-                
+
                 titulo = titulo.split('\t').join('');
                 titulo = titulo.split('\n').join('');
 
-               
+
 
                 //Busco los precios
-                if(estiloViejo){
+                if (estiloViejo) {
+                    var spans = $('span.price-tag-fraction');
+                    var precio = '$ '.concat($(spans[0]).text());
 
-                }else{
-
-                
-
-                var priceTags = $('#short-desc').find('span.price-tag-fraction');
-
-                var precio;
-
-                //Si hay dos, es porque esta en oferta y el segundo es el precio actual
-
-                if (priceTags.length > 1) {
-                    precio = '$ '.concat($(priceTags[1]).text());
                 } else {
-                    precio = '$ '.concat($(priceTags[0]).text());
+
+
+                    var priceTags = $('#short-desc').find('span.price-tag-fraction');
+
+                    var precio;
+
+                    //Si hay dos, es porque esta en oferta y el segundo es el precio actual
+
+                    if (priceTags.length > 1) {
+                        precio = '$ '.concat($(priceTags[1]).text());
+                    } else {
+                        precio = '$ '.concat($(priceTags[0]).text());
+                    }
+
                 }
-
-               }
-
-                const siteHeading = $('ul.questions__list');
-
+                
                 var $stock = false;
                 var $fecha, $fechaTemprana;
+                var $respuestaDefinitiva = '';
+                
+               if(!estiloViejo){ 
+
+                const siteHeading = $('ul.questions__list');
 
                 let limite = 0;
 
                 var preguntasRespuestas = siteHeading.find('li');
-
-                var $respuestaDefinitiva='';
 
                 var dosSemanas = new Date();
                 var aux = dosSemanas.getDate() - 14;
                 dosSemanas.setDate(aux);
 
                 $fechaTemprana = dosSemanas;
-
-                
 
                 for (let index = 0; index < preguntasRespuestas.length && index < 5; index++) {
 
@@ -81,7 +81,7 @@ async function createArticle(url) {
                         $zonarespuesta = $(preguntasRespuestas[index]).find('article.questions__item--answer');
 
                         //Obtengo la fecha y hora en texto, y me quedo con la fecha en string
-                        $elementoTime =  $zonarespuesta.find('time');
+                        $elementoTime = $zonarespuesta.find('time');
                         $fecha = $elementoTime.text();
                         $fecha = $fecha.substring(0, ($fecha.indexOf(':') - 3));
                         $fecha = $fecha.split('/').join('-');
@@ -115,7 +115,7 @@ async function createArticle(url) {
                                     }
 
 
-                                   
+
 
                                     $fecha = $elementoTime.text().substring(0, 10);
 
@@ -129,7 +129,7 @@ async function createArticle(url) {
                                             $respuestaDefinitiva = $respuesta;
                                         }
 
-                                        
+
 
                                         $fecha = $elementoTime.text().substring(0, 10);
                                     } else {
@@ -143,14 +143,18 @@ async function createArticle(url) {
                         }
                     }
                 }
+            }
 
-                resolve({'titulo':titulo, 
-                         'precio': precio, 
-                         'stock': $stock, 
-                         'fecha': $fecha, 
-                         'respuesta': $respuestaDefinitiva, 
-                         'url': url, 
-                         'imagen': imagenLink});
+                resolve({
+                    'titulo': titulo,
+                    'precio': precio,
+                    'stock': $stock,
+                    'fecha': $fecha,
+                    'respuesta': $respuestaDefinitiva,
+                    'url': url,
+                    'imagen': imagenLink,
+                    'ecommerce': 'Mercadolibre'
+                });
             } else {
                 resolve(null);
                 return;
