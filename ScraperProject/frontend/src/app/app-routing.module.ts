7cbce613152e5aaba46c21165ed382795f21ec92
Franco Raniolo <franiolo@pasante06.sis.cooperativaobrera.coop>
2020-02-17 17:25:04 -0300

    ruta detalles articulo

--- ScraperProject/frontend/src/app/app-routing.module.ts
+++ ScraperProject/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetalleArticuloComponent } from './components/detalle-articulo/detalle-articulo.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: SidenavComponent},
+  { path: 'detalleArticulo', component: DetalleArticuloComponent},
+  { path: '**', component: SidenavComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
