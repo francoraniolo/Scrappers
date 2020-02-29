a0c8f4b239d77ac7e80abb276d242bc66b43c512
francoraniolo <franco11-09@hotmail.com>
2020-02-29 03:28:13 -0300

    para que ande github pages

--- ScraperProject/server/index.js
+++ ScraperProject/server/index.js
@@ -13,8 +13,28 @@ app.use(express.json());
 
 //REQUESTS PERMITIDOS DESDE
 app.use(cors({
-    origin: 'http://localhost:4200'
+    origin: ['http://localhost:4200',
+        'https://francoraniolo.github.io'
+    ]
 }));
+
+
+// var allowedOrigins = ['http://localhost:4200',
+//     'https://francoraniolo.github.io/Scrappers/'
+// ];
+// app.use(cors({
+//     origin: function(origin, callback) {
+//         // allow requests with no origin 
+//         // (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 //Routes
 app.use('/api/articulos', require('./routes/articulos.routes'));
